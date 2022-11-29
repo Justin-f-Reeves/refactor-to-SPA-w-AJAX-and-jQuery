e9e28b71d8269f137c8a5e33ed22a98a454d8fa0
Justin Reeves <mail.to.justin@gmail.com>
2022-11-29 00:59:56 -0500

    add search functionality

--- app.js
+++ app.js
@@ -24,19 +24,64 @@ app.get("/", function (req, res) {
   res.redirect("/todos");
 });
 
-//original index route 
+//Current index route 
+
+// function to be used in the .get("/todos", ..) route
+// this allows us to escape any special characters with a backslash
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+// index route updated for adding search functionality
 
 app.get("/todos", function (req, res) {
-  Todo.find({}, function (err, todos) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("index", {
-        todos: todos
-      });
-    }
-  })
+  if (req.query.keyword) { // if there's a query string called keyword then..
+    // set the constant (variable) regex equal to a new regular expression created from the keyword 
+    // that we pulled from the query string
+    const regex = new RegExp(escapeRegex(req.query.keyword), 'gi');
+    // query the database for Todos with text property that match the regular expression version of the search keyword
+    Todo.find({
+      text: regex
+    }, function (err, todos) {
+      if (err) {
+        console.log(err);
+      } else {
+        // send back the todos we found as JSON
+        res.json(todos);
+      }
+    });
+  } else {
+    // if there wasn't any query string keyword then..
+    Todo.find({}, function (err, todos) { // query the db for all todos
+      if (err) {
+        console.log(err);
+      } else {
+        if (req.xhr) { // if request was made with AJAX then ..
+          res.json(todos); // send back all todos as JSON
+        } else {
+          res.render("index", {
+            todos: todos
+          }); // otherwise render the index view and pass in all todos with EJS
+        }
+      }
+    });
+  }
 });
+
+
+// Completed SPA index route
+
+// app.get("/todos", function (req, res) {
+//   Todo.find({}, function (err, todos) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.render("index", {
+//         todos: todos
+//       });
+//     }
+//   })
+// });
 
 // test index route for our ajax.js file
 
@@ -104,7 +149,9 @@ app.get("/todos/:id/edit", function (req, res) {
 });
 
 app.put("/todos/:id", function (req, res) {
-  Todo.findByIdAndUpdate(req.params.id, req.body.todo, {new: true}, function (err, todo) {
+  Todo.findByIdAndUpdate(req.params.id, req.body.todo, {
+    new: true
+  }, function (err, todo) {
     if (err) {
       console.log(err);
     } else {
