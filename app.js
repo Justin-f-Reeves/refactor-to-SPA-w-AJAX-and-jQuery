a4f872231c31c50a43b20c802881e8cf2ca4bd9e
Justin Reeves <mail.to.justin@gmail.com>
2022-11-29 00:33:25 -0500

    add API endpoints to all route handlers. draft ajax requests in client-side js file. modify index page markup.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express = require("express"),
-app     = express(),
-mongoose = require("mongoose"),
-bodyParser = require("body-parser"),
-expressSanitizer = require("express-sanitizer"),
-methodOverride = require('method-override');
+express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  expressSanitizer = require("express-sanitizer"),
+  methodOverride = require('method-override');
 
 mongoose.connect("mongodb://localhost/todo_app");
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(expressSanitizer());
 app.set("view engine", "ejs");
 app.use(methodOverride('_method'));
@@ -18,70 +20,113 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model("Todo", todoSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.redirect("/todos");
 });
 
-app.get("/todos", function(req, res){
-  Todo.find({}, function(err, todos){
-    if(err){
+//original index route 
+
+app.get("/todos", function (req, res) {
+  Todo.find({}, function (err, todos) {
+    if (err) {
       console.log(err);
     } else {
-      res.render("index", {todos: todos}); 
+      res.render("index", {
+        todos: todos
+      });
     }
   })
 });
 
-app.get("/todos/new", function(req, res){
- res.render("new"); 
+// test index route for our ajax.js file
+
+// app.get("/todos", function(req, res){
+//   Todo.find({}, function(err, todos){
+//     if(err){
+//       console.log(err);
+//     } else {
+//       if (req.xhr) {
+//       res.json(todos);
+//       } else {
+//       res.render("index", {todos: todos}); 
+//       }
+//     }
+//   })
+// });
+
+
+
+app.get("/todos/new", function (req, res) {
+  res.render("new");
 });
 
-app.post("/todos", function(req, res){
- req.body.todo.text = req.sanitize(req.body.todo.text);
- var formData = req.body.todo;
- Todo.create(formData, function(err, newTodo){
-    if(err){
-      res.render("new");
+// old POST route
+
+// app.post("/todos", function(req, res){
+//  req.body.todo.text = req.sanitize(req.body.todo.text);
+//  var formData = req.body.todo;
+//  Todo.create(formData, function(err, newTodo){
+//     if(err){
+//       res.render("new");
+//     } else {
+//         res.redirect("/todos");
+//     }
+//   });
+// });
+
+
+// new POST route
+
+app.post("/todos", function (req, res) {
+  req.body.todo.text = req.sanitize(req.body.todo.text);
+  var formData = req.body.todo;
+  Todo.create(formData, function (err, newTodo) {
+    if (err) {
+      res.render("/");
     } else {
-        res.redirect("/todos");
+      res.json(newTodo);
     }
   });
 });
 
-app.get("/todos/:id/edit", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-     res.redirect("/")
-   } else {
-      res.render("edit", {todo: todo});
-   }
- });
+
+app.get("/todos/:id/edit", function (req, res) {
+  Todo.findById(req.params.id, function (err, todo) {
+    if (err) {
+      console.log(err);
+      res.redirect("/")
+    } else {
+      res.render("edit", {
+        todo: todo
+      });
+    }
+  });
 });
 
-app.put("/todos/:id", function(req, res){
- Todo.findByIdAndUpdate(req.params.id, req.body.todo, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
-      res.redirect('/');
-   }
- });
-});
-
-app.delete("/todos/:id", function(req, res){
- Todo.findById(req.params.id, function(err, todo){
-   if(err){
-     console.log(err);
-   } else {
-      todo.remove();
-      res.redirect("/todos");
-   }
- }); 
+app.put("/todos/:id", function (req, res) {
+  Todo.findByIdAndUpdate(req.params.id, req.body.todo, {new: true}, function (err, todo) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
 });
 
 
-app.listen(3000, function() {
+
+app.delete("/todos/:id", function (req, res) {
+  Todo.findByIdAndRemove(req.params.id, function (err, todo) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(todo);
+    }
+  });
+});
+
+
+app.listen(3000, function () {
   console.log('Server running on port 3000');
 });
 
